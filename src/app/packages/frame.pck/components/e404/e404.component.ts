134aeb50dffc5f4832eb4130c65c0926abca2aac
Imran Khan <imransilvake@gmail.com>
2019-11-10 21:14:25 +0100

    404 - 05

--- src/app/packages/frame.pck/components/e404/e404.component.ts
+++ src/app/packages/frame.pck/components/e404/e404.component.ts
@@ -1,5 +1,7 @@
 // angular
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 // app
 import page404 from '../../../../../assets/data/other/404';
@@ -11,23 +13,42 @@ import { HelperService } from '../../../utilities.pck/accessories.mod/services/h
 	styleUrls: ['./e404.component.scss']
 })
 
-export class E404Component implements OnInit {
-	@ViewChild('eye', { static: false }) eye: ElementRef;
-
+export class E404Component implements OnInit, OnDestroy {
 	public page404 = page404;
 	public x;
 	public y;
 
+	private _ngUnSubscribe: Subject<void> = new Subject<void>();
+
 	ngOnInit() {
 		HelperService.detectMouseMove()
+			.pipe(takeUntil(this._ngUnSubscribe))
 			.subscribe(event => {
 				// event['pageX']: get horizontal coordinates of the mouse
 				// window.innerWidth: browser width
-				this.x = event['pageX'] * 100 / window.innerWidth + '%';
+				const x = event['pageX'] * 100 / window.innerWidth;
 
 				// event['pageY']: vertical coordinates of the mouse
 				// window.innerHeight: browser height
-				this.y = event['pageY'] * 100 / window.innerHeight + '%';
+				const y = event['pageY'] * 100 / window.innerHeight;
+
+				// left: x > 20
+				// right: x > 80
+				if (x > 20 && x < 80) {
+					this.x = `${x}%`;
+				}
+
+				// top: y > 30
+				// bottom: y > 60
+				if (y > 30 && y < 60) {
+					this.y = `${y}%`;
+				}
 			});
+	}
+
+	ngOnDestroy() {
+		// remove subscriptions
+		this._ngUnSubscribe.next();
+		this._ngUnSubscribe.complete();
 	}
 }
