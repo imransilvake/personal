dd32bd55f711db6e6d1e7aa9a6de193b00ed5ec3
Imran Khan <imransilvake@gmail.com>
2020-04-16 23:12:28 +0200

    delete projects assets

--- src/assets/data/projects/projects.ts
+++ src/assets/data/projects/projects.ts
@@ -41,13 +41,15 @@ const projects = {
 					text: ['SCSS']
 				}
 			},
-			badge: 'SCSS',
+			badges: {
+				code: 'SCSS'
+			},
 			controls: {
+				lock: false,
 				recent: true,
 				filter: 'scss'
 			},
 			actions: {
-				lock: false,
 				link: `${AppLinks.projects.github.profile}${AppLinks.projects.github.list.scss.name}`,
 				website: AppLinks.projects.github.list.scss.link
 			}
@@ -64,54 +66,17 @@ const projects = {
 					text: ['HTML5', 'SCSS', 'TypeScript', 'Angular', 'Material', 'MomentJs', 'RxJs', 'Store']
 				}
 			},
-			badge: 'Angular',
+			badges: {
+				code: 'Angular',
+				private: 'Projects.Terms.Private'
+			},
 			controls: {
+				lock: true,
 				recent: true,
 				filter: 'angular'
 			},
 			actions: {
-				lock: true,
-				gallery: [
-					{ src: '/assets/images/projects/ham/00a.png' },
-					{ src: '/assets/images/projects/ham/00b.png' },
-					{ src: '/assets/images/projects/ham/00c.png' },
-					{ src: '/assets/images/projects/ham/01.png' },
-					{ src: '/assets/images/projects/ham/02.png' },
-					{ src: '/assets/images/projects/ham/02a.png' },
-					{ src: '/assets/images/projects/ham/03.png' },
-					{ src: '/assets/images/projects/ham/04.png' },
-					{ src: '/assets/images/projects/ham/05.png' },
-					{ src: '/assets/images/projects/ham/06.png' },
-					{ src: '/assets/images/projects/ham/07.png' },
-					{ src: '/assets/images/projects/ham/08.png' },
-					{ src: '/assets/images/projects/ham/09.png' },
-					{ src: '/assets/images/projects/ham/10.png' },
-					{ src: '/assets/images/projects/ham/11.png' },
-					{ src: '/assets/images/projects/ham/12.png' },
-					{ src: '/assets/images/projects/ham/13.png' },
-					{ src: '/assets/images/projects/ham/14.png' },
-					{ src: '/assets/images/projects/ham/14a.png' },
-					{ src: '/assets/images/projects/ham/15.png' },
-					{ src: '/assets/images/projects/ham/16.png' },
-					{ src: '/assets/images/projects/ham/17.png' },
-					{ src: '/assets/images/projects/ham/18.png' },
-					{ src: '/assets/images/projects/ham/19.png' },
-					{ src: '/assets/images/projects/ham/20.png' },
-					{ src: '/assets/images/projects/ham/21.png' },
-					{ src: '/assets/images/projects/ham/22.png' },
-					{ src: '/assets/images/projects/ham/23.png' },
-					{ src: '/assets/images/projects/ham/24.png' },
-					{ src: '/assets/images/projects/ham/25.png' },
-					{ src: '/assets/images/projects/ham/26.png' },
-					{ src: '/assets/images/projects/ham/27.png' },
-					{ src: '/assets/images/projects/ham/28.png' },
-					{ src: '/assets/images/projects/ham/29.png' },
-					{ src: '/assets/images/projects/ham/30.png' },
-					{ src: '/assets/images/projects/ham/31.png' },
-					{ src: '/assets/images/projects/ham/32.png' },
-					{ src: '/assets/images/projects/ham/33.png' },
-					{ src: '/assets/images/projects/ham/34.png' }
-				]
+				galleryName: 'ham'
 			}
 		},
 		{
@@ -126,37 +91,17 @@ const projects = {
 					text: ['HTML5', 'SCSS', 'TypeScript', 'Angular', 'Material', 'MomentJs', 'RxJs', 'Store']
 				}
 			},
-			badge: 'Angular',
+			badges: {
+				code: 'Angular',
+				private: 'Projects.Terms.Private'
+			},
 			controls: {
+				lock: true,
 				recent: true,
 				filter: 'angular'
 			},
 			actions: {
-				lock: true,
-				gallery: [
-					{ src: '/assets/images/projects/hga/01.png' },
-					{ src: '/assets/images/projects/hga/02.png' },
-					{ src: '/assets/images/projects/hga/03.png' },
-					{ src: '/assets/images/projects/hga/04.png' },
-					{ src: '/assets/images/projects/hga/05.png' },
-					{ src: '/assets/images/projects/hga/06.png' },
-					{ src: '/assets/images/projects/hga/07.png' },
-					{ src: '/assets/images/projects/hga/08.png' },
-					{ src: '/assets/images/projects/hga/09.png' },
-					{ src: '/assets/images/projects/hga/10.png' },
-					{ src: '/assets/images/projects/hga/11.png' },
-					{ src: '/assets/images/projects/hga/12.png' },
-					{ src: '/assets/images/projects/hga/13.png' },
-					{ src: '/assets/images/projects/hga/14.png' },
-					{ src: '/assets/images/projects/hga/15.png' },
-					{ src: '/assets/images/projects/hga/16.png' },
-					{ src: '/assets/images/projects/hga/17.png' },
-					{ src: '/assets/images/projects/hga/18.png' },
-					{ src: '/assets/images/projects/hga/19.png' },
-					{ src: '/assets/images/projects/hga/20.png' },
-					{ src: '/assets/images/projects/hga/21.png' },
-					{ src: '/assets/images/projects/hga/22.png' }
-				]
+				galleryId: 'hga'
 			}
 		},
 		{
@@ -171,27 +116,17 @@ const projects = {
 					text: ['HTML5', 'SCSS', 'TypeScript', 'Angular', 'Material', 'MomentJs', 'RxJs', 'Store']
 				}
 			},
-			badge: 'Angular',
+			badges: {
+				code: 'Angular',
+				private: 'Projects.Terms.Private'
+			},
 			controls: {
+				lock: true,
 				recent: false,
 				filter: 'angular'
 			},
 			actions: {
-				lock: true,
-				gallery: [
-					{ src: '/assets/images/projects/rabt-portal/01.png' },
-					{ src: '/assets/images/projects/rabt-portal/02.png' },
-					{ src: '/assets/images/projects/rabt-portal/03.png' },
-					{ src: '/assets/images/projects/rabt-portal/04.png' },
-					{ src: '/assets/images/projects/rabt-portal/05.png' },
-					{ src: '/assets/images/projects/rabt-portal/05a.png' },
-					{ src: '/assets/images/projects/rabt-portal/06.png' },
-					{ src: '/assets/images/projects/rabt-portal/07.png' },
-					{ src: '/assets/images/projects/rabt-portal/08.png' },
-					{ src: '/assets/images/projects/rabt-portal/09.png' },
-					{ src: '/assets/images/projects/rabt-portal/10.png' },
-					{ src: '/assets/images/projects/rabt-portal/11.png' }
-				]
+				galleryId: 'rabt-portal'
 			}
 		},
 		{
@@ -206,40 +141,17 @@ const projects = {
 					text: ['HTML5', 'SCSS', 'TypeScript', 'Angular', 'NativeScript', 'Material', 'MomentJs', 'RxJs', 'Store']
 				}
 			},
-			badge: 'NativeScript',
+			badges: {
+				code: 'NativeScript',
+				private: 'Projects.Terms.Private'
+			},
 			controls: {
+				lock: true,
 				recent: false,
 				filter: 'angular'
 			},
 			actions: {
-				lock: true,
-				gallery: [
-					{ src: '/assets/images/projects/hsa/01.png' },
-					{ src: '/assets/images/projects/hsa/02.png' },
-					{ src: '/assets/images/projects/hsa/03.png' },
-					{ src: '/assets/images/projects/hsa/04.png' },
-					{ src: '/assets/images/projects/hsa/05.png' },
-					{ src: '/assets/images/projects/hsa/06.png' },
-					{ src: '/assets/images/projects/hsa/07.png' },
-					{ src: '/assets/images/projects/hsa/08.png' },
-					{ src: '/assets/images/projects/hsa/09.png' },
-					{ src: '/assets/images/projects/hsa/10.png' },
-					{ src: '/assets/images/projects/hsa/11.png' },
-					{ src: '/assets/images/projects/hsa/12.png' },
-					{ src: '/assets/images/projects/hsa/13.png' },
-					{ src: '/assets/images/projects/hsa/14.png' },
-					{ src: '/assets/images/projects/hsa/15.png' },
-					{ src: '/assets/images/projects/hsa/16.png' },
-					{ src: '/assets/images/projects/hsa/17.png' },
-					{ src: '/assets/images/projects/hsa/18.png' },
-					{ src: '/assets/images/projects/hsa/19.png' },
-					{ src: '/assets/images/projects/hsa/20.png' },
-					{ src: '/assets/images/projects/hsa/21.png' },
-					{ src: '/assets/images/projects/hsa/22.png' },
-					{ src: '/assets/images/projects/hsa/23.png' },
-					{ src: '/assets/images/projects/hsa/24.png' },
-					{ src: '/assets/images/projects/hsa/25.png' }
-				]
+				galleryId: 'hsa'
 			}
 		},
 		{
@@ -254,24 +166,17 @@ const projects = {
 					text: ['HTML5', 'SCSS', 'React', 'Material', 'Firebase', 'Redux', 'MomentJs', 'i18next', 'React Color']
 				}
 			},
-			badge: 'React',
+			badges: {
+				code: 'React'
+			},
 			controls: {
+				lock: false,
 				recent: true,
 				filter: 'react'
 			},
 			actions: {
-				lock: false,
 				link: `${AppLinks.projects.github.profile}${AppLinks.projects.github.list.slackClone.name}`,
-				gallery: [
-					{ src: '/assets/images/projects/slack-app-clone/01.png' },
-					{ src: '/assets/images/projects/slack-app-clone/02.png' },
-					{ src: '/assets/images/projects/slack-app-clone/03.png' },
-					{ src: '/assets/images/projects/slack-app-clone/04.png' },
-					{ src: '/assets/images/projects/slack-app-clone/05.png' },
-					{ src: '/assets/images/projects/slack-app-clone/06.png' },
-					{ src: '/assets/images/projects/slack-app-clone/07.png' },
-					{ src: '/assets/images/projects/slack-app-clone/08.png' }
-				]
+				galleryId: 'slack-app-clone'
 			}
 		},
 		{
@@ -286,20 +191,17 @@ const projects = {
 					text: ['HTML5', 'SCSS', 'React', 'Material', 'Firebase', 'Redux', 'MomentJs', 'i18next']
 				}
 			},
-			badge: 'React',
+			badges: {
+				code: 'React'
+			},
 			controls: {
+				lock: false,
 				recent: true,
 				filter: 'react'
 			},
 			actions: {
-				lock: false,
 				link: `${AppLinks.projects.github.profile}${AppLinks.projects.github.list.takeAwayChallenge.name}`,
-				gallery: [
-					{ src: '/assets/images/projects/takeaway-challenge/01.png' },
-					{ src: '/assets/images/projects/takeaway-challenge/02.png' },
-					{ src: '/assets/images/projects/takeaway-challenge/03.png' },
-					{ src: '/assets/images/projects/takeaway-challenge/04.png' }
-				],
+				galleryId: 'takeaway-challenge',
 				website: AppLinks.projects.github.list.takeAwayChallenge.link
 			}
 		},
@@ -315,13 +217,15 @@ const projects = {
 					text: ['HTML5', 'SCSS', 'React']
 				}
 			},
-			badge: 'React',
+			badges: {
+				code: 'React'
+			},
 			controls: {
+				lock: false,
 				recent: false,
 				filter: 'react'
 			},
 			actions: {
-				lock: false,
 				link: `${AppLinks.projects.github.profile}${AppLinks.projects.github.list.ticTacToe.name}`,
 			}
 		},
@@ -337,22 +241,17 @@ const projects = {
 					text: ['HTML5', 'SCSS', 'PHP', 'OXID Framework']
 				}
 			},
-			badge: 'PHP',
+			badges: {
+				code: 'PHP'
+			},
 			controls: {
+				lock: false,
 				recent: false,
 				filter: 'php'
 			},
 			actions: {
-				lock: false,
 				link: `${AppLinks.projects.github.profile}${AppLinks.projects.github.list.multiSiteSlider.name}`,
-				gallery: [
-					{ src: '/assets/images/projects/multisite-slider/01.png' },
-					{ src: '/assets/images/projects/multisite-slider/02.png' },
-					{ src: '/assets/images/projects/multisite-slider/03.png' },
-					{ src: '/assets/images/projects/multisite-slider/04.png' },
-					{ src: '/assets/images/projects/multisite-slider/05.png' },
-					{ src: '/assets/images/projects/multisite-slider/06.png' }
-				]
+				galleryId: 'multisite-slider'
 			}
 		}
 	]
