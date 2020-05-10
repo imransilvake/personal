7a8203397ec85c6c3ba12a0da8951662bd2668d2
Imran Khan <imransilvake@gmail.com>
2020-05-10 20:26:26 +0200

    update header

--- src/app/packages/frame.pck/components/header/header.component.ts
+++ src/app/packages/frame.pck/components/header/header.component.ts
@@ -1,12 +1,9 @@
 // angular
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 // app
 import { ROUTING } from '../../../../../environments/environment';
-import { LocalStorageItems } from '../../../../../app.config';
-import { faFont } from '@fortawesome/free-solid-svg-icons';
-import { StorageService } from '../../../core.pck/storage.mod/services/storage.service';
 import navigation from 'src/assets/data/common/navigation';
 
 @Component({
@@ -15,21 +12,11 @@ import navigation from 'src/assets/data/common/navigation';
 	styleUrls: ['./header.component.scss']
 })
 
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 	public routing = ROUTING;
-	public faIcons = [faFont];
 	public navigation = navigation;
-	public fontSizeInactive = true;
 
-	constructor(
-		private _router: Router,
-		private _storageService: StorageService
-	) {
-	}
-
-	ngOnInit() {
-		// setup font size
-		this.onClickFontSizeToggle(true);
+	constructor(private _router: Router) {
 	}
 
 	/**
@@ -38,24 +25,5 @@ export class HeaderComponent implements OnInit {
 	 */
 	public isActiveRoute(link) {
 		return this._router.isActive(link, false);
-	}
-
-	/**
-	 * toggle font size: standard / large
-	 * @param init
-	 */
-	public onClickFontSizeToggle(init?: boolean) {
-		// get fontSizeMode from local storage
-		const fontSizeMode = this._storageService.get(LocalStorageItems.fontSizeMode) || 'normal';
-		const reverse = (fontSizeMode === 'normal') ? 'ik-zoom' : 'normal';
-		const value = init ? fontSizeMode : reverse;
-		this.fontSizeInactive = value === 'ik-zoom';
-
-		// add data attribute to html
-		const html = document.getElementsByTagName('html')[0];
-		html.setAttribute('class', value);
-
-		// update to local storage
-		this._storageService.put(LocalStorageItems.fontSizeMode, value);
 	}
 }
--- src/app/packages/frame.pck/frame.module.ts
+++ src/app/packages/frame.pck/frame.module.ts
@@ -7,10 +7,11 @@ import { SharedModule } from '../../shared.module';
 import { E404Component } from './components/e404/e404.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { ScrollTopComponent } from './components/scroll-top/scroll-top.component';
 import { LanguageSwitchComponent } from './components/header/language-switch/language-switch.component';
-import { PushNotificationComponent } from './components/push-notification/push-notification.component';
 import { ThemeSwitchComponent } from './components/header/theme-switch/theme-switch.component';
+import { FontSwitchComponent } from './components/header/font-switch/font-switch.component';
+import { ScrollTopComponent } from './components/scroll-top/scroll-top.component';
+import { PushNotificationComponent } from './components/push-notification/push-notification.component';
 
 @NgModule({
 	imports: [
@@ -21,10 +22,11 @@ import { ThemeSwitchComponent } from './components/header/theme-switch/theme-swi
 		E404Component,
 		HeaderComponent,
 		FooterComponent,
-		ScrollTopComponent,
 		LanguageSwitchComponent,
-		PushNotificationComponent,
-		ThemeSwitchComponent
+		ThemeSwitchComponent,
+		FontSwitchComponent,
+		ScrollTopComponent,
+		PushNotificationComponent
 	],
 	exports: [
 		HeaderComponent,
