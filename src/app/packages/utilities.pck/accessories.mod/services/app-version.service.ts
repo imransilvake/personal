7a19c364dcf39377b8fd35ad2b264b8077ea2e49
Imran Khan <imransilvake@gmail.com>
2020-10-18 22:07:57 +0200

    update version manual

--- src/app/packages/utilities.pck/accessories.mod/services/app-version.service.ts
+++ src/app/packages/utilities.pck/accessories.mod/services/app-version.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class AppVersionService {
-	public version = '1.0.1';
+	public version = '1.0.2';
 
 	/**
 	 * get app version
