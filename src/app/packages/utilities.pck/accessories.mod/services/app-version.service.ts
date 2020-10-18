90d71e35f7afd473266632bd8010fcd2f51085a9
Imran Khan <imransilvake@gmail.com>
2020-10-18 22:15:40 +0200

    fix: handle automatic version

--- src/app/packages/utilities.pck/accessories.mod/services/app-version.service.ts
+++ src/app/packages/utilities.pck/accessories.mod/services/app-version.service.ts
@@ -1,14 +1,17 @@
 // angular
 import { Injectable } from '@angular/core';
 
+// app
+import { version } from '../../../../../../package.json';
+
 @Injectable({ providedIn: 'root' })
 export class AppVersionService {
-	public version = '1.0.2';
+	public cVersion = version;
 
 	/**
 	 * get app version
 	 */
 	get getAppVersion() {
-		return this.version;
+		return this.cVersion;
 	}
 }
